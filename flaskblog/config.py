597bfee16be9eb060283a85ffaedb41f6e4d7ad1
AkshatJain-AJ <akshat.bareilly@gmail.com>
2021-10-13 10:30:16 +0530

    Initial commit

---
+++ flaskblog/config.py
@@ -0,0 +1,10 @@
+import os
+
+class Config:
+	SECRET_KEY = '7d6b133c1cb44efcbf17855f1d542969'
+	SQLALCHEMY_DATABASE_URI = 'sqlite:///site.db'
+	MAIL_SERVER = 'smtp.googlemail.com'
+	MAIL_PORT = 587
+	MAIL_USE_TLS = True
+	MAIL_USERNAME = os.environ.get('EMAIL_USER')
+	MAIL_PASSWORD = os.environ.get('EMAIL_PASS')
---
+++ readme.md
@@ -0,0 +1,9 @@
+TestUser@demo.com
+demo
+
+akshat@testing.com
+welcome
+
+Note:
+1. facing issue for 'forgot password and password reset operation' but implementation is complete.
+2. Few here and there fixes are required.
