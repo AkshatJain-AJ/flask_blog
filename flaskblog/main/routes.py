b723e74f6baf38e93bb48f2ee9e0e8492e66d99a
AkshatJain-AJ <akshat.bareilly@gmail.com>
2021-10-14 18:01:32 +0530

    added search filter
    1. side menu only avaliable from logining
    2. added search filter

--- flaskblog/main/routes.py
+++ flaskblog/main/routes.py
@@ -15,3 +15,24 @@ def home():
 def about():
 	return render_template('about.html', title='About')
 
+@main.route("/searchText")
+def searchText():
+	page = request.args.get('page', 1, type=int)
+	
+	search_input = request.args.get('search-input')
+	search_input = f'%{search_input}%'
+
+	posts = Post.query.filter(Post.title.contains(search_input)).order_by(Post.date_posted.desc()).paginate(page=page, per_page=5)
+
+	return render_template('home.html', posts=posts)		
+
+@main.route("/searchDate")
+def searchDate():
+	page = request.args.get('page', 1, type=int)
+
+	date_input = request.args.get('date-input')
+	date_input = f'%{date_input}%'
+
+	posts = Post.query.filter(Post.date_posted.contains(date_input)).order_by(Post.date_posted.desc()).paginate(page=page, per_page=5)
+
+	return render_template('home.html', posts=posts)	
